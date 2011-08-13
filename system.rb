982c4704a281f7b32f2f64ad8bbef493ca00b988
Robin Wenglewski <robin@wenglewski.de>
2011-08-13 21:58:39 +0200

    configured macvim

--- system.rb
+++ system.rb
@@ -1,5 +1,5 @@
 dep "rw sys" do
-  requires 'configured zsh', "configured git", "macvim"
+  requires 'configured zsh', "configured git", "configured macvim"
 end
 
 dep "configured git" do
@@ -32,5 +32,24 @@ dep "oh-my-zsh" do
   met? {"~/.oh-my-zsh".p.exists?}
   meet do
       shell "git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh"
+  end
+end
+
+dep "configured macvim" do
+  requires "macvim"
+  
+  met? { "~/.vim/".p.exists? }
+  meet do
+    log_shell "cloning dotvim", "git clone https://rweng@github.com/rweng/dotvim.git ~/.vim"
+    log_shell "cloning vundle", "git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
+    
+    # this doesn't work, blocks, so please do it manually
+    # log_shell "installing vim bundles", "vim -u ~/.vim/vundle.vim -U NONE +BundleInstall +q"
+    
+    log "please run the following command and press enter: " + "'vim -u ~/.vim/vundle.vim -U NONE +BundleInstall +q'"
+    STDIN.gets
+    
+    log_shell "building", "cd ~/.vim/bundle/Command-T/ruby/command-t;ruby extconf.rb;make"
+    shell "ln -sf ~/.vim/vimrc ~/.vimrc"
   end
 end
