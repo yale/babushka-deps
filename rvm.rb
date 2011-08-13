5ce9209d82a13bfeafc92a6d8aa1118ff3d8c383
Robin Wenglewski <robin@wenglewski.de>
2011-08-14 01:31:57 +0200

    rvm deps

---
+++ rvm.rb
@@ -0,0 +1,86 @@
+dep 'rvm' do
+  requires 'rvm base', 'rvm globals', 'rvm default'
+end
+
+
+def rvm_script
+  "source ~/.rvm/scripts/rvm;"
+end
+
+def rvm_run cmd
+  `#{rvm_script}#{cmd}`
+end
+
+def current_rubies
+	out = rvm_run("rvm list rubies")
+	rubies = out.scan(/^[=> ]{3}([^ ]+) /).flatten
+end
+
+dep 'rvm default' do
+	requires 'rvm base'
+	define_var :default_ruby, :choices => current_rubies, :message => "Which ruby do you what to use as default?"
+	
+	met?{rvm_run("rvm current")[/system/] == nil}
+	meet do
+		rvm_run("rvm use #{var(:default_ruby)} --default")
+	end
+end
+
+dep 'rvm base' do
+  met? {
+    "~/.rvm/scripts/rvm".p.file?
+  }
+
+  meet {
+    shell 'bash -c "`curl https://rvm.beginrescueend.com/install/rvm`"'
+  }
+end
+
+dep 'rvm globals' do
+  requires 'rvm base'
+
+  define_var :rubyies, :default => "ruby-1.9.2", :message => "which rubies do you want to create? (seperate by ,)"
+  define_var :gems, :default => "bundler, rake, gemedit, powder", :message => "which gems do you want to install into global? (seperate by ,)"
+
+
+  def rubies
+    var(:rubies).split(/ *, */)
+  end
+
+  def gems
+    var(:gems).split(/ *, */)
+  end
+
+  met? {
+    # are all required rubies installed?
+    ruby_list = `#{rvm_script} rvm list rubies`
+    missing = rubies.select{|r| ruby_list[/#{r}/] == nil}
+    unless missing.empty?
+      false
+    else
+      # are all required gems installed?
+      result = true;
+      rubies.each do |r|
+        list = `#{rvm_script} rvm use #{r}@global;gem list`
+
+        # are all gems in the gemset?
+        missing = gems.select{|e| list[/#{e}/] == nil}
+        result = false and break unless missing.empty?
+      end
+    end
+  }
+
+  meet {
+    # log("run: rvm install 1.9.2") and STDIN.gets
+
+    rubies.each do |r|
+      log "installing ruby: #{r}"
+      rvm_run "rvm install #{r}"
+      log "installing gems"
+      rvm_run "rvm use --create #{r}@global;" +
+        "gem install #{gems.join(' ')}"
+    end
+
+  }
+end
+
