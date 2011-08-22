6da0c984c39fe79866bccc1be5e6dc59c0d0dede
Robin Wenglewski <robin@wenglewski.de>
2011-08-22 12:58:56 +0200

    adding ack dependency

--- managed.rb
+++ managed.rb
@@ -1,3 +1,5 @@
 dep 'macvim', :template => 'managed' do
   provides 'mvim'
 end
+
+dep 'ack', :template => 'managed'
--- system.rb
+++ system.rb
@@ -1,5 +1,12 @@
 dep "rw sys" do
-  requires 'configured zsh', "configured git", "configured macvim", 'pow'
+  requires 'configured zsh', "configured git", "configured macvim", 'pow', "user in wheel group"
+end
+
+dep "user in wheel group" do
+  define_var :user, :default => shell("whoami"), :message => "Which user do you want to add to the wheel group?"
+
+  met?{shell("groups")[/(^| )wheel($| )/]}
+  meet{shell "usermod -a -G wheel #{var(:user)}"}
 end
 
 dep "pow" do
@@ -11,7 +18,7 @@ end
 
 dep "configured git" do
   requires "git"
-  
+
   met? {"~/.gitconfig".p.exists?}
   meet do
     render_erb "render/gitconfig.erb", :to => "~/.gitconfig".p
@@ -19,8 +26,8 @@ dep "configured git" do
 end
 
 dep "configured zsh" do
-  requires 'rw dotfiles', 'benhoskings:zsh', "oh-my-zsh"
-  
+  requires 'rw dotfiles', 'benhoskings:zsh', "oh-my-zsh", 'ack'
+
   met?{"~/.zshrc".p.exists?}
   meet do
     shell "ln -sf ~/.dotfiles/zsh ~/.zsh"
@@ -38,24 +45,24 @@ end
 dep "oh-my-zsh" do
   met? {"~/.oh-my-zsh".p.exists?}
   meet do
-      shell "git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh"
+    shell "git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh"
   end
 end
 
 dep "configured macvim" do
   requires "macvim"
-  
+
   met? { "~/.vim/".p.exists? }
   meet do
     log_shell "cloning dotvim", "git clone https://rweng@github.com/rweng/dotvim.git ~/.vim"
     log_shell "cloning vundle", "git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle"
-    
+
     # this doesn't work, blocks, so please do it manually
     # log_shell "installing vim bundles", "vim -u ~/.vim/vundle.vim -U NONE +BundleInstall +q"
-    
+
     log "please run the following command and press enter: " + "'vim -u ~/.vim/vundle.vim -U NONE +BundleInstall +q'"
     STDIN.gets
-    
+
     log_shell "building", "cd ~/.vim/bundle/Command-T/ruby/command-t;ruby extconf.rb;make"
     shell "ln -sf ~/.vim/vimrc ~/.vimrc"
     shell "ln -sf ~/.vim/gvimrc ~/.gvimrc"
