240b8204b6ac98206b53843211ad22dee2296c03
Yale Spector <yspector@mdsol.com>
2012-04-02 17:20:57 -0300

    Update rvm.rb

--- rvm.rb
+++ rvm.rb
@@ -20,7 +20,7 @@ dep 'rvm installed' do
   }
 
   meet {
-    shell 'bash -c "`curl https://rvm.beginrescueend.com/install/rvm`"'
+    shell 'bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)'
   }
 end
 
